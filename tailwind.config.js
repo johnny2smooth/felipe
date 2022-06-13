f5bd24a99c7207a28a07a48051cf7e74fbc38872
johnny2smooth <johnbp13@gmail.com>
2022-06-13 15:30:20 -0700

    header looking better. mobile looks good too

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{md,html}'],
   theme: {
+    colors: {
+      blueWater: '#4969ED',
+      whiteLight: '#F8F7F3',
+      orangePeel: '#F89066',
+    },
     extend: {},
   },
   plugins: [],
