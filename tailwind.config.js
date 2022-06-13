4fc26a624cbc1d187e15dfba206a01b908931f2a
johnny2smooth <johnbp13@gmail.com>
2022-06-13 15:57:15 -0700

    colors added to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{md,html}'],
   theme: {
@@ -6,6 +8,13 @@ module.exports = {
       blueWater: '#4969ED',
       whiteLight: '#F8F7F3',
       orangePeel: '#F89066',
+      gray: colors.slate,
+      black: colors.black,
+      white: colors.white,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
     },
     extend: {},
   },
